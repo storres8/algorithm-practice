245b59c37b093ea4d58b9877b3b16e4b14ea60ce
storres8 <stevetorres7562@gmail.com>
2020-02-23 17:46:55 -0500

    update.

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,6 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, StringB) {}
+function anagrams(stringA, stringB) {}
 
 module.exports = anagrams;
